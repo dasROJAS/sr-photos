56a30e6aea13f50b99bb0a973d51daa9773277de
Steve Campos <campos.coder@gmail.com>
2018-08-07 13:11:41 -0700

    firestore data added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { AngularFirestore } from 'angularfire2/firestore'
+import { Observable } from 'rxjs'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+    photos: Observable<any[]>;
+
+    constructor(private db: AngularFirestore) {
+        this.photos = db.collection('photos').valueChanges();
+    }
+
+    addPhoto() {
+        const name = document.getElementById('photoName').value
+        const path = document.getElementById('photoPath').value
+        const now = Date.now()
+
+        this.db.collection('photos').add({
+            'name': name,
+            'path': path,
+            'createdAt': now
+        })
+        .then(ref => console.log('ref': ref))
+        .catch(err => console.log('err': err))
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        AngularFireAuthModule,
+        AngularFireStorageModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
